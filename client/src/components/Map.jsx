c4e99184529ff5ece7cf57a9485bde7a0b85e75b
Derrick <juneparkoh@gmail.com>
2023-03-04 15:02:47 -0800

    google map rendered and button for curr loction
    Co-authored-by: clark-pang <sondelius@gmail.com>
    Co-authored by: DavidMNorman <DavidMNorman@gmail.com>
    Co-authored by: jon19200 <jon19200@gmail.com>

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,23 +2,47 @@ import '../stylesheets/App.css';
 import React, { useState } from 'react';
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
+// import * as dotenv from 'dotenv'
+// dotenv.config()
 // require('dotenv').config;
 
+// const script = document.createElement('script');
+// script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+// script.async = true;
+
 function Map() {
+  const [mapPos, setMapPos] = useState({ lat: 44, lng: -80 });
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    googleMapsApiKey: 'AIzaSyBCOm76ZZYuU7YSbYUmDRwhdj8XTW5K5jk',
   });
 
   if (!isLoaded) return <div>Loading...</div>;
 
+  const currPosition = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+          // change map center positioning state
+          setMapPos(pos);
+        },
+      );
+    }
+  };
+
   return (
-    <GoogleMap
-      zoom={10}
-      center={{ lat: 44, lng: -80 }}
-      mapContainerClassName="map-container"
-    >
-      {/* Place Holder */}
-    </GoogleMap>
+    <div>
+      <input type="button" onClick={(e)=>currPosition()} />
+      <GoogleMap
+        zoom={10}
+        center={mapPos}
+        mapContainerClassName="map-container"
+      >
+      </GoogleMap>
+    </div>
   );
 }
 
