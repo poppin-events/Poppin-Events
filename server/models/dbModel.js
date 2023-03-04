223a7481473ce1ff3be80d1630d9b273aa46226a
DavidMNorman <DavidMNorman@gmail.com>
2023-03-04 11:48:43 -0800

    add connection to PostgreSQL server and query function

---
+++ server/models/dbModel.js
@@ -0,0 +1,15 @@
+const { Pool } = require('pg');
+
+const PG_URI = 'postgres://leekwldv:85zQjHG1-BdoK3wOIfWOGl63DL85OW9c@mahmud.db.elephantsql.com/leekwldv';
+
+// create new pool
+const pool = new Pool({
+  connectionString: PG_URI,
+});
+
+module.exports = {
+  query: (text, params, callback) => {
+    console.log('executing query', text);
+    return pool.query(text, params, callback);
+  },
+};
