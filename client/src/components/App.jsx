6011993cdc98908828cb1a2ac34673186a1beafc
Clark <116928935+clark-pang@users.noreply.github.com>
2023-03-03 20:57:12 -0800

    Update App.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,11 +2,8 @@ import '../stylesheets/App.css';
 import axios from 'axios';
 import React, { useState } from 'react';
 
-console.log('derrick');
-
 function App() {
   const [data, setData] = useState();
-  console.log('Jon was here'); // test2
   const urlWithProxy = '/api/v1';
 
   function getDataFromServer() {
@@ -21,10 +18,8 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={getDataFromServer}>Access server using proxy TEST1Test3</button>
-      <p></p>
+      <button onClick={getDataFromServer}>Access server using proxy</button>
       <p>data is : {data}</p>
-      <p>Clark, Derrick, and Jonathan are pretty cool</p>
     </div>
   );
 }
