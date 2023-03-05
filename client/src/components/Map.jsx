a0ad2f923100df32aabccba90ff413c3515469a0
Jonathan Klibansky <jon19200@gmail.com>
2023-03-05 17:36:48 -0500

    Small changes to Map
    Co-authored-by: clark-pang <sondelius@gmail.com>
    Co-authored by: DavidMNorman <DavidMNorman@gmail.com>
    Co-authored by: Igniphis <juneparkoh@gmail.com>
    Co-authored-by: Derrick Oh <Igniphis@users.noreply.github.com>
    Co-authored-by: Clark <clark-pang@users.noreply.github.com>

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -10,17 +10,13 @@ function Map() {
   const [mapPos, setMapPos] = useState({ lat: 44, lng: -80 });
   const [markerData, setMarkerData] = useState([]);
   const [eventData, setEventData] = useState({title: "No Party", creator: "Derrick", location: {lat: 37.772, lng: -122.214}, date: "Dec 11", description: "ITS WORKING"});
-  const [map, setMap] = useState(null);
 
   const { isLoaded } = useJsApiLoader({
     googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
 
-  // if (!isLoaded) return <div>Loading...</div>;
-
-  const markers = []; // every element is a marker jsx element
   // get all markers from database
-  const data = [{title: "Party Time", creator: "Derrick", location: {lat: 44, lng: -80}, date: "Dec 11", description: "ITS WORKING"}, {title: "Party #2", creator: "Derrick", location: {lat: 43, lng: -80}, date: "Jan 1", description: "HELLO"}, {title: "Party 3!!!", creator: "Jonathan", location: {lat: 42, lng: -80}, date: "March 5th", description: "WORKING TEST!!!!"}];
+  // const data = [{title: "Party Time", creator: "Derrick", location: {lat: 44, lng: -80}, date: "Dec 11", description: "ITS WORKING"}, {title: "Party #2", creator: "Derrick", location: {lat: 43, lng: -80}, date: "Jan 1", description: "HELLO"}, {title: "Party 3!!!", creator: "Jonathan", location: {lat: 42, lng: -80}, date: "March 5th", description: "WORKING TEST!!!!"}];
 
   useEffect(() => {
     fetch('/api/events')
@@ -69,7 +65,6 @@ function Map() {
           <Marker title={place.title} position={place.location} onClick={() => eventBox(place)} />
         ))}
     </GoogleMap>
-      {map}
     </div>
   );
 }
