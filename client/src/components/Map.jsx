043a05f794a0e54f62608acd77c19081b593ac90
Derrick <juneparkoh@gmail.com>
2023-03-04 16:51:22 -0800

    currently trying to add markers
    Co-authored-by: clark-pang <sondelius@gmail.com>
    Co-authored by: DavidMNorman <DavidMNorman@gmail.com>
    Co-authored by: jon19200 <jon19200@gmail.com>

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,10 +1,6 @@
 import '../stylesheets/App.css';
-import React, { useState } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-
-// import * as dotenv from 'dotenv'
-// dotenv.config()
-// require('dotenv').config;
+import React, { useState, useEffect } from 'react';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 
 // const script = document.createElement('script');
 // script.src = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
@@ -12,11 +8,34 @@ import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 function Map() {
   const [mapPos, setMapPos] = useState({ lat: 44, lng: -80 });
+  const [markerData, setMarkerData] = useState([]);
   const { isLoaded } = useJsApiLoader({
-    googleMapsApiKey: 'AIzaSyBCOm76ZZYuU7YSbYUmDRwhdj8XTW5K5jk',
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY,
   });
 
   if (!isLoaded) return <div>Loading...</div>;
+
+  const markers = []; // every element is a marker jsx element
+  // get all markers from database
+  useEffect(() => {
+    fetch('/api/markers')
+      .then((res) => res.json())
+      .then((data) => {
+        setMarkerData(data);
+      });
+  }, []); // [{name : "name", location:}, {name : "name", location:}, {name : "name", location:}]
+
+  const eventBox = () => {
+    // TODO: change state for info box stuff when marker is clicked
+  };
+
+  // for every marker from database invoke a function that creates a marker jsx element
+  markerData.forEach((el) => {
+    const {
+      title, creator, location, date, description } = el;
+      // convert location string to lat and lng
+    markers.push(<Marker title={title} position={location} onClick={() => eventBox()} />);
+  });
 
   const currPosition = () => {
     if (navigator.geolocation) {
@@ -35,12 +54,17 @@ function Map() {
 
   return (
     <div>
-      <input type="button" onClick={(e)=>currPosition()} />
+      <div className="info">
+        Our info window on top of the map
+        Change state will reflect changes to the text in this div
+      </div>
+      <input type="button" onClick={() => currPosition()} />
       <GoogleMap
         zoom={10}
         center={mapPos}
         mapContainerClassName="map-container"
       >
+        {markers}
       </GoogleMap>
     </div>
   );
